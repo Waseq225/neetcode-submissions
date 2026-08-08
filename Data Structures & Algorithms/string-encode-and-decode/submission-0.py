bdaf7f5f8ab4ff28e960b5e7140a9f365e0a1393
Mohammad Waseq <51850180+Waseq225@users.noreply.github.com>
2026-08-08 15:20:13 +1000

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,21 @@
+import base64
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = []
+        for char in strs:
+            res.append(str(len(char)))
+            res.append('*')
+            res.append(char)
+        return "".join(res)
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != '*':
+                j += 1
+            length = int(s[i:j])
+            res.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return res
